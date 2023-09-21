903bb8b3eb9c96e452e553f80d2c4bf2b548f387
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-21 19:18:10 +0300

    custom notes item done and font added

---
+++ lib/views/widgets/custom_notes.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class CustomNotesItem extends StatelessWidget {
+  const CustomNotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 180,
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: const Row(
+        children: [
+          LeftSection(),
+          Spacer(),
+          RightSection(),
+        ],
+      ),
+    );
+  }
+}
+
+class LeftSection extends StatelessWidget {
+  const LeftSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: SizedBox(
+        width: 180,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Flutter Tips',
+              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                  ),
+            ),
+            const SizedBox(
+              height: 18,
+            ),
+            Text(
+              'Build your carrer with confidence',
+              maxLines: 6,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Colors.black.withOpacity(0.3),
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RightSection extends StatelessWidget {
+  const RightSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 28.0, right: 20.0, bottom: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.delete,
+              size: 28,
+              color: Colors.black,
+            ),
+          ),
+          const Spacer(),
+          Text(
+            'Sep 21/2023',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Colors.black.withOpacity(0.3),
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_notes.dart';
 
 class NotesScreenBody extends StatelessWidget {
   const NotesScreenBody({super.key});
@@ -23,46 +24,5 @@ class NotesScreenBody extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-class CustomNotesItem extends StatelessWidget {
-  const CustomNotesItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 200,
-      margin: const EdgeInsets.only(bottom: 10),
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: const Row(
-        children: [
-          LeftSection(),
-          RightSection(),
-        ],
-      ),
-    );
-  }
-}
-
-class LeftSection extends StatelessWidget {
-  const LeftSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column();
-  }
-}
-
-class RightSection extends StatelessWidget {
-  const RightSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column();
   }
 }
