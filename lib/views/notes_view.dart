0db4fe692399a4bbba40aedaea6ecae0a06b1a5a
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-21 19:39:56 +0300

    floating action button added

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -9,6 +9,24 @@ class NotesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: NotesScreenBody(),
+      floatingActionButton: CustomFloatingActionButton(),
+    );
+  }
+}
+
+class CustomFloatingActionButton extends StatelessWidget {
+  const CustomFloatingActionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {},
+      child: const Center(
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+      ),
     );
   }
 }
