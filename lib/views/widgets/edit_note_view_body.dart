54b36201c6e77dc222064dfae311ae9761bfb905
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-23 22:23:57 +0300

    edit view screen done

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_hive/views/widgets/custom_app_bar.dart';
+import 'package:todo_hive/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -17,6 +18,17 @@ class EditNoteViewBody extends StatelessWidget {
           CustomAppBar(
             text: 'Edit Notes',
             isSearch: false,
+          ),
+          SizedBox(
+            height: 44,
+          ),
+          CustomTextField(hint: 'Title'),
+          SizedBox(
+            height: 20,
+          ),
+          CustomTextField(
+            hint: 'Content',
+            maxLines: 5,
           ),
         ],
       ),
