f09316500301aebb5ce78b1def3c99962761f253
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-24 16:57:20 +0300

    add addnote cubit to model sheet

---
+++ lib/views/widgets/add_note_form.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'custom_button.dart';
+import 'custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      autovalidateMode: autovalidateMode,
+      key: formKey,
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          CustomTextField(
+            hint: 'Title',
+            onSaved: (value) {
+              title = value;
+              return null;
+            },
+          ),
+          const SizedBox(height: 20),
+          CustomTextField(
+            hint: 'content',
+            maxLines: 5,
+            onSaved: (value) {
+              subtitle = value;
+              return null;
+            },
+          ),
+          const SizedBox(height: 40),
+          CustomButton(
+            onPressed: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/bottom_sheet_widget.dart
+++ lib/views/widgets/bottom_sheet_widget.dart
@@ -1,72 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:todo_hive/cubits/add_note_cubit/add_note_cubit.dart';
 
-import 'custom_button.dart';
-import 'custom_text_field.dart';
+import 'add_note_form.dart';
 
 class CustomModelSheetWidget extends StatelessWidget {
   const CustomModelSheetWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
-      child: AddNoteForm(),
-    );
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-
-  String? title, subtitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      autovalidateMode: autovalidateMode,
-      key: formKey,
-      child: ListView(
-        shrinkWrap: true,
-        children: [
-          CustomTextField(
-            hint: 'Title',
-            onSaved: (value) {
-              title = value;
-              return null;
-            },
-          ),
-          const SizedBox(height: 20),
-          CustomTextField(
-            hint: 'content',
-            maxLines: 5,
-            onSaved: (value) {
-              subtitle = value;
-              return null;
-            },
-          ),
-          const SizedBox(height: 40),
-          CustomButton(
-            onPressed: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteFailure) {
+            print(state.error);
+          }
+          if (state is AddNoteSuccess) {
+            Navigator.pop(context);
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+            inAsyncCall: state == AddNoteLoading() ? true : false,
+            child: const AddNoteForm(),
+          );
+        },
       ),
     );
   }
