9942bff03dbc25559c5b5cf4c574275d6c72f339
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-24 14:28:34 +0300

    notes model created

---
+++ lib/models/notes_model.dart
@@ -0,0 +1,12 @@
+class NotesMdoel {
+  final String title;
+  final String description;
+  final String date;
+  final int color;
+
+  NotesMdoel(
+      {required this.title,
+      required this.description,
+      required this.date,
+      required this.color});
+}
